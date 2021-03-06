67a3d2a7f5b239ab238ea546f7a97b3d8a3e71eb
vovanky <vovantriem1965@gmail.com>
2021-03-06 09:30:31 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('foo', function () {
+    return 'Hello World';
+});
+Route::get('user/{id}', function ($id = 1) {
+    return 'User '.$id;
+});
+Route::prefix('admin')->group(function(){
+    Route::get('user/{id}', function ($id = 1) {
+        return 'User '.$id;
+    });
+});
